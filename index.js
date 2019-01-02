66bee1efad173aa4ca2094fe9cef471b2ffbb48e
Claire Muller <clairermuller@gmail.com>
2019-01-02 01:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   
   if (cardNumber) {
-    return `Your total cost is $${total()}, which will be charged to the card 83296759.`;
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
 
   return "Sorry, we don't have a credit card on file for you.";
